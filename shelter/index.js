70b68b8c3973906d62fd87036d0e809d1c1a53e7
Ruzanna <ruzanna30a@gmail.com>
2024-09-12 22:34:53 +0400

    feat: add overlay and no-scroll functionality

--- shelter/index.js
+++ shelter/index.js
@@ -1,5 +1,31 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const body = document.body;
+    // const burgerMenu = document.getElementById('burger');
+    // const closeMenuButton = document.querySelector('close-menu');
+    // const header = document.querySelector('header');
+
     document.getElementById('burger').addEventListener('click', function() {
         document.querySelector('header').classList.toggle('open')
+        body.classList.toggle('no-scroll');
+
     })
+    document.getElementById('menu').addEventListener('click', function() {
+        document.querySelector('header').classList.toggle('open')
+        body.classList.toggle('no-scroll');
+    })
+    
+
+    // function openMenu() {
+    //     header.classList.add('open');
+        
+    // }
+
+    // function closeMenu() {
+    //     header.classList.remove('open');
+    //     body.classList.remove('no-scroll');
+    // }
+
+    // burgerMenu.addEventListener('click', openMenu);
+    // closeMenuButton.addEventListener('click', closeMenu);
+
 })
