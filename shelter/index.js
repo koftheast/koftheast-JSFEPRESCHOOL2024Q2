027ac3b0c82f96881eb7681a566de21c67ea7c77
Ruzanna <ruzanna30a@gmail.com>
2024-10-11 17:57:05 +0400

    fix: update htlm/css code

--- shelter/index.js
+++ shelter/index.js
@@ -1,9 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
     const body = document.body;
-    // const burgerMenu = document.getElementById('burger');
-    // const closeMenuButton = document.querySelector('close-menu');
-    // const header = document.querySelector('header');
-
     document.getElementById('burger').addEventListener('click', function() {
         document.querySelector('header').classList.toggle('open')
         body.classList.toggle('no-scroll');
@@ -13,19 +9,5 @@ document.addEventListener("DOMContentLoaded", function() {
         document.querySelector('header').classList.toggle('open')
         body.classList.toggle('no-scroll');
     })
-    
-
-    // function openMenu() {
-    //     header.classList.add('open');
-        
-    // }
-
-    // function closeMenu() {
-    //     header.classList.remove('open');
-    //     body.classList.remove('no-scroll');
-    // }
-
-    // burgerMenu.addEventListener('click', openMenu);
-    // closeMenuButton.addEventListener('click', closeMenu);
 
 })
